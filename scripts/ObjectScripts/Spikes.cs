bfef885a1a4dba4dfc9e6198de149bb828c071af
MicPlays <micplays115@gmail.com>
2025-02-13 17:29:38 -0500

    silly spikes

--- scripts/ObjectScripts/Spikes.cs
+++ scripts/ObjectScripts/Spikes.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Spikes : GameObject
+public partial class Spikes : SolidObject
 {
     public enum Facing
     {
@@ -11,11 +11,37 @@ public partial class Spikes : GameObject
         Down
     }
     [Export]
+    public NodePath spritePath;
+    public Sprite2D sprite;
+    [Export]
     public Facing facing {get; set;}
     public override void _Ready()
     {
+        base._Ready();
+        sprite = GetNode<Sprite2D>(spritePath);
         hitbox = GetNode<Area2D>(hitboxPath);
         hitbox.AreaEntered += OnAreaEnter;
+        switch (facing)
+        {
+            case Facing.Left:
+                sprite.RotationDegrees = 270;
+                hitbox.Position += new Vector2(-16, 0);
+                hitbox.RotationDegrees = 270;
+                break;
+            case Facing.Right:
+                sprite.RotationDegrees = 90;
+                hitbox.Position += new Vector2(-16, 0);
+                hitbox.RotationDegrees = 90;
+                break;
+            case Facing.Up:
+                sprite.RotationDegrees = 0;
+                hitbox.Position += new Vector2(0, -16);
+                break;
+            case Facing.Down:
+                sprite.RotationDegrees = 180;
+                hitbox.Position += new Vector2(0, 16);
+                break;   
+        }
     }
 
     public void OnAreaEnter(Area2D area)
@@ -29,11 +55,11 @@ public partial class Spikes : GameObject
                 switch (facing)
                 {
                     case Facing.Left:
-                        if (attackable.parentObject.xSpeed < 0)
+                        if (attackable.parentObject.xSpeed <= 0)
                             otherObject.Damage();
                         break;
                     case Facing.Right:
-                        if (attackable.parentObject.xSpeed > 0)
+                        if (attackable.parentObject.xSpeed >= 0)
                             otherObject.Damage();
                         break;
                     case Facing.Up:
